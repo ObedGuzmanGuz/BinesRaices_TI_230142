b0ca13cb709db9e0d6b8b6b51b60476feb9254ed
Obed Guzman Flores <230142@utxicotepec.edu.mx>
2024-12-12 15:42:05 -0600

    Act02
    Implementar su paleta de colores, tipografías , y diseño de componentes

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,16 @@
 export default {
   content: ['./views/**/*.pug'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        white: '#FFFFFF',
+        black: '#000000',
+        cerulean: '#006992',
+        mint:'#62A87C',
+        orange: '#F3A712',
+      },},
   },
+  darkMode: 'class',
   plugins: [],
 }
 
